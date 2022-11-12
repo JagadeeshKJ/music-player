e6934b13576e7e112f95d512f8bac2da5daf78c5
JagadeeshKJ <jagadeesh10th@gmail.com>
2022-11-12 19:42:37 +0530

    first commit

---
+++ src/components/Loader.jsx
@@ -0,0 +1,16 @@
+import { loader } from '../assets';
+
+const Loader = ({ title }) => (
+  <div className='w-full flex flex-col justify-center items-center'>
+    <img 
+      src={loader}
+      alt="loader"
+      className='w-32 h-32 object-contain' 
+    />
+    <h1 className="font-bold text-2xl text-white mt-2">
+      {title || "Loading..."}
+    </h1>
+  </div>
+);
+
+export default Loader;
